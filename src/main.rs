d7f1d4b7605f62d04f7202c9ab9775a797e43388
sketchc89@gmail.com <christopher.sketch@gm.com>
2019-12-16 21:33:15 -0500

    use getopts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate getopts;
+use getopts::Options;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("f", "file", "set input file name", "NAME");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(arg) => { arg }
+        Err(err) => { panic!(err.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let input = matches.opt_str("f");
 }
